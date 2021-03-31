8f7670b4082f5bc565069b0e2a27c81561290425
Matthieu Poirot <matthieu.poirot1@gmail.com>
2021-03-31 16:56:46 +0200

    changed api url

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -7,7 +7,7 @@ import {Observable} from 'rxjs';
 })
 export class ApiService {
 
-  API_URL = 'http://localhost:3000';
+  API_URL = 'http://104.238.188.46:3000';
 
   constructor(private http: HttpClient) { }
 
